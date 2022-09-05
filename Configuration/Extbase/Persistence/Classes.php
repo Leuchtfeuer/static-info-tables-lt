ce2da880fa04b5dd416519be55179a618ae86c66
Max Rösch <github@elbebass.com>
2022-09-05 12:31:52 +0200

    [TASK] Add release script and adapt header comments

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the "Static Info Tables (AR)" extension for TYPO3 CMS.
+ * This file is part of the "Static Info Tables (LT)" extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
